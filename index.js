2d47ebb0c6d1372caf3148ed9c4357724363d52c
StephynVex <100586342+StephynVex@users.noreply.github.com>
2022-03-28 13:55:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const iniciar = async(auth) => {
                         
                         switch (command) {
 
-case (body == ('hola')): {
+case 'hola':
 vex.sendMessage(from, 'tu nariz contra mis bolas.....digo hola,como estas', text, {quoted : mek})
 break
 
