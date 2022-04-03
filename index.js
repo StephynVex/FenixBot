1df1d7416b0ab2aafff65eb1b60d862f8b157700
StephynVex <100586342+StephynVex@users.noreply.github.com>
2022-04-02 21:27:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -94,6 +94,15 @@ case 'onichan':
 vex.sendMessage(from, fs.readFileSync('./media/AUD-20220302-WA0205.mp3'), audio, {quoted: mek, ptt: true, mimetype: 'audio/mp4'})
 
 break
+{quoted: {key: {
+fromMe: false,
+participant: '0@s.whatsapp.net'. ...(from? {
+remoteJid: "status@broadcast" } : {})
+}.
+message: {
+"documentMessage": {"title": "SephynVex🍷", 'jpegThumbnail:
+fs.readFileSync('./media/fenix.png')}}
+}}
 
                                 default:
                                         if (body.startsWith('>')){
